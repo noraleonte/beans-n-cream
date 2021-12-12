40b03544632ab49f001e7c4875b1ffb7dbca62e6
Nora Leonte <nora.leonte@evozon.com>
2021-12-13 00:59:27 +0200

    linked button to about page

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
 import coffee from '../../assets/coffee.png';
 import '../../styles/home.scss';
+import paths from '../../constants/paths.js';
 const Hero = () => {
   return (
     <div className='hero d-flex align-itmes-center'>
       <div className='left d-flex flex-column justify-content-center'>
         <h2 className='text-light'>The world's best flavours in one place</h2>
         <button className='btn btn-info text-primary rounded-pill'>
-          Read more about us
+          <Link to={paths.ABOUT}>Read more about us</Link>
         </button>
       </div>
       <img src={coffee} alt='coffee' className='coffee-img' />
